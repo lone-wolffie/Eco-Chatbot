0cf9ade46d12f1447ef4c0af972cb630f316a5ec
lone-wolffie <karenwanjiru09@gmail.com>
2025-09-20 01:10:31 +0300

    Add .gitignore

--- public/script.js
+++ public/script.js
@@ -67,7 +67,7 @@ async function sendMessage(message) {
   addTypingIndicator();
 
   try {
-    const res = await fetch("http://localhost:5000/api/chat", {
+    const res = await fetch("http://localhost:3000/send", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message })
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 import fetch from "node-fetch";
+import dotenv from "dotenv";
+dotenv.config();
 
 
 const app = express();
@@ -10,6 +13,55 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+// Hugging Face API configuration
+const HF_API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-large";
+
+const HF_HEADERS = {
+    "Authorization": `Bearer ${process.env.HF_ACCESS_TOKEN}`,
+    "Content-Type": "application/json"
+};
+
+app.get("/", async (req, res) => {
+    const result = res.json(
+        {ok: true});
+    console.log(result);
+});
+
+app.post("/send", async (req, res) => {
+    try {
+        const {message, parameters} = req.body;
+
+        if (!message) {
+            return res.status(400).json({
+                error: "Input text is required"
+            });
+        }
+
+        const payload = {
+            inputs: message,
+            parameters: parameters || {}
+        };
+
+        const response = await axios.post(HF_API_URL, payload, { 
+            headers: HF_HEADERS });
+        
+        let modelReply = "🤖 Sorry, I couldn't generate a reply.";
+
+        if (Array.isArray(response.data) && response.data[0].generated_text) {
+        modelReply = response.data[0].generated_text;
+        }
+
+        res.json({ reply: modelReply });
+
+
+    } catch (error) {
+        console.error("Error calling Hugging Face API:", error.response ? error.response.data : error.message);
+        res.status(500).json({ error: "Failed to process request with Hugging Face API." });
+    }
+
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
